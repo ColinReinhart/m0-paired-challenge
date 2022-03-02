3081ce37756ea7aeeca715f87875bd52299d69cc
Colin Reinhart <colinbreinhartgmail.com>
2022-03-01 20:56:00 -0700

    Added Ruby File and Index

---
+++ challenge/index.md
@@ -0,0 +1,8 @@
+# Books That Colin and Sarah Like
++ A Psalm For The Wild Born Few
++ Storm Glass
++ A Gentleman In Moscow
++ Howl's Moving Castle
++ Fable
+## Work Flow Explanation:
+> Colin drove and Sarah navigated, but we worked more collaboratively than a traditional driver/navigator. We got stuck when we wanted to put in an array before we realized that we would be asked to on the next bullet point. The only thing we had to look up was spelling. No disagreements.  
---
+++ challenge/ruby_file.rb
@@ -0,0 +1,8 @@
+# We chose the name ruby_file.rb because we were running out of time and it was easy
+books_smell_good = true #this line declares a variable as the boolean true
+books_checked_out = 150
+books_read = 153.25 # if you were partially completed with a book it would be a float
+local_library = "Athmar Park Library"
+books_in_stock = 2067 #They have several books
+library_card = false #This line indicates the user does not have a library card
+genres = ["Sci Fi", "Fantasy", "Psychological Thirller", "Mystery", "Drama", "Horror", "Adventure"]
